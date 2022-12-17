2dd68cc329c6f9197f662ed8c70345759513e7e9
patelaftab <10patelaftab@gmail.com>
2022-12-17 08:35:14 +0530

    Using Switch Case Cheking Wage And Attendence

--- EmpWageProgram/EmployeeAttendance.cs
+++ EmpWageProgram/EmployeeAttendance.cs
@@ -10,27 +10,27 @@ namespace EmpWageProgram
     {
         public static void CheckAttendance()
         {
-            int IS_FULL_TIME = 1;
-            int IS_PART_TIME = 2;
+           const int IS_FULL_TIME = 1;
+           const int IS_PART_TIME = 2;
             int EMP_WAGE_PER_HR = 20;
             int emphrs = 0;
             int empwage = 0;
             Random random= new Random();
             int empcheck = random.Next(0, 3);
-            if(empcheck == IS_FULL_TIME)
+            switch(empcheck)
             {
+                case IS_FULL_TIME:
                 Console.WriteLine("Employee is Full Time Present");
                 emphrs = 8;
-            }
-            else if(empcheck==IS_PART_TIME)
-            {
+                    break;
+                case IS_PART_TIME:
                 Console.WriteLine("Employee is Part Time Present");
                 emphrs = 4;
-            }
-            else 
-            {
+                    break;
+                default:
                 Console.WriteLine("Employee is Absent");
                 emphrs = 0;
+                    break;
             }
             empwage = emphrs * EMP_WAGE_PER_HR;
             Console.WriteLine("Employee Wage is :" +empwage);
