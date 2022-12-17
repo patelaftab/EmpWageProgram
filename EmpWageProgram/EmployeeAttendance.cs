c80f01bcbb3b49be51ac9f060ba9aacc1867a491
patelaftab <10patelaftab@gmail.com>
2022-12-17 08:24:23 +0530

    Cheking Wage And Attendence For An EMployee

--- EmpWageProgram/EmployeeAttendance.cs
+++ EmpWageProgram/EmployeeAttendance.cs
@@ -11,17 +11,25 @@ namespace EmpWageProgram
         public static void CheckAttendance()
         {
             int IS_FULL_TIME = 1;
+            int IS_PART_TIME = 2;
             int EMP_WAGE_PER_HR = 20;
             int emphrs = 0;
             int empwage = 0;
             Random random= new Random();
-            int empcheck = random.Next(0, 2);
+            int empcheck = random.Next(0, 3);
             if(empcheck == IS_FULL_TIME)
             {
+                Console.WriteLine("Employee is Full Time Present");
                 emphrs = 8;
+            }
+            else if(empcheck==IS_PART_TIME)
+            {
+                Console.WriteLine("Employee is Part Time Present");
+                emphrs = 4;
             }
             else 
             {
+                Console.WriteLine("Employee is Absent");
                 emphrs = 0;
             }
             empwage = emphrs * EMP_WAGE_PER_HR;
